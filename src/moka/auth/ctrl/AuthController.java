43ffbd52940c594928b72a2ac558de2663a0d420
moka <gaoyuanfell@sina.com>
2017-08-06 14:45:19 +0800

    1

--- src/moka/auth/ctrl/AuthController.java
+++ src/moka/auth/ctrl/AuthController.java
@@ -1,9 +1,11 @@
 package moka.auth.ctrl;
 
 import moka.auth.service.AuthService;
+import moka.auth.to.AuthTo;
 import moka.auth.vo.AuthVo;
 import moka.basic.ctrl.BasicController;
 import moka.basic.log4j.LoggerService;
+import moka.basic.page.Page;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/auth")
@@ -28,6 +31,41 @@ public class AuthController extends BasicController {
     private Object insert(@RequestBody AuthVo vo){
         String i = authService.insert(vo);
         return result(i);
+    }
+
+    /**
+     * 查 分页
+     */
+    @RequestMapping(value = "findPage.htm")
+    @ResponseBody
+    public Object findPage(@RequestBody AuthVo vo) {
+        Page list = authService.findPage(vo);
+        return result(list);
+    }
+
+    /**
+     * 查
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "findOne.htm")
+    @ResponseBody
+    public Object findOne(String id) {
+        AuthTo to = authService.findOne(id);
+        return result(to);
+    }
+
+    /**
+     * 作用与下拉
+     * @param vo
+     * @return
+     */
+    @RequestMapping(value = "findUseSelect.htm")
+    @ResponseBody
+    public Object findUseSelect(@RequestBody AuthVo vo) {
+        List<AuthTo> list = authService.findUseSelect(vo);
+        return result(list);
     }
 
 }
